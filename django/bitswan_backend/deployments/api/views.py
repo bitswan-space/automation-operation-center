88e4701fc7b3d583986c546ea4adc8786881c424
Tomáš Peroutka <67205912+Pero-coder@users.noreply.github.com>
2025-06-24 13:18:54 +0200

    Refactor to add envs to bitswan-backend.env instead of docker-compose.yml (#99) (#98)

--- django/bitswan_backend/deployments/api/views.py
+++ django/bitswan_backend/deployments/api/views.py
@@ -59,8 +59,17 @@ class PipelineIDEStartView(KeycloakMixin, APIView):
         )
 
 def current_deployed_version(request):
-    return JsonResponse({
-        "aoc"            : os.getenv("AOC"), 
-        "bitswan-backend": os.getenv("BITSWAN_BACKEND"), 
-        "profile-manager": os.getenv("PROFILE_MANAGER")
-    })
+    versions = {}
+    if os.getenv("AOC_VERSION"):
+        versions["aoc"] = os.getenv("AOC_VERSION")
+    
+    if os.getenv("BITSWAN_BACKEND_VERSION"):
+        versions["bitswan-backend"] = os.getenv("BITSWAN_BACKEND_VERSION")
+    
+    if os.getenv("PROFILE_MANAGER_VERSION"):
+        versions["profile-manager"] = os.getenv("PROFILE_MANAGER_VERSION")
+
+    if os.getenv("KEYCLOAK_VERSION"):
+        versions["keycloak"] = os.getenv("KEYCLOAK_VERSION")
+
+    return JsonResponse(versions)
