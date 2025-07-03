a3c5dd40de37797868e2668db32b129ee9811c51
Mike Farad <mike18farad@gmail.com>
2025-07-03 14:54:00 +0300

    fix: update command syntax and formatting issues

--- aoc_cli/update.py
+++ aoc_cli/update.py
@@ -1,15 +1,19 @@
-import click
 import subprocess
+
+import click
+import requests
+
 from aoc_cli.env.config import Environment, InitConfig
 from aoc_cli.handlers.init import InitCommand
-import requests
+
 
 @click.command()
 @click.option("--from", "from_url", default=None, type=str, help="URL endpoint returning versions for update")
 def update(from_url):
-    aoc_image=None
-    aoc_be_image=None
-    profile_manager_image=None
+
+    aoc_image = None
+    aoc_be_image = None
+    profile_manager_image = None
 
     if from_url:
         response = requests.get(from_url)
@@ -17,17 +21,19 @@ def update(from_url):
             versions = response.json()
 
             if versions.get("aoc"):
-                aoc_image = f"bitswan/automation-operations-centre:{versions["aoc"]}"
-            if versions.get("bitswan-backend"): 
-                aoc_be_image = f"bitswan/bitswan-backend:{versions["bitswan-backend"]}"
-            if versions.get("profile-manager"): 
-                profile_manager_image = f"bitswan/profile-manager:{versions["profile-manager"]}"
+                aoc_image = f"bitswan/automation-operations-centre:{versions['aoc']}"
+            if versions.get("bitswan-backend"):
+                aoc_be_image = f"bitswan/bitswan-backend:{versions['bitswan-backend']}"
+            if versions.get("profile-manager"):
+                profile_manager_image = (
+                    f"bitswan/profile-manager:{versions['profile-manager']}"
+                )
 
     init_config = InitConfig(
-        env=Environment.PROD, 
-        aoc_image=aoc_image, 
-        aoc_be_image=aoc_be_image, 
-        profile_manager_image=profile_manager_image
+        env=Environment.PROD,
+        aoc_image=aoc_image,
+        aoc_be_image=aoc_be_image,
+        profile_manager_image=profile_manager_image,
     )
     init_command = InitCommand(init_config)
 
@@ -35,5 +41,6 @@ def update(from_url):
 
     subprocess.run(
         ["docker", "compose", "-f", "docker-compose.yml", "up", "-d"],
-        cwd=init_config.aoc_dir, check=True
+        cwd=init_config.aoc_dir,
+        check=True,
     )
