320747bd51850baf5cce38571e0ed4818d4e8155
Mike Farad <mike18farad@gmail.com>
2025-07-17 02:23:38 +0300

    feat: add AOC_BUILD_NO environment variable and display in Dashboard

--- nextjs/src/app/dashboard/layout.tsx
+++ nextjs/src/app/dashboard/layout.tsx
@@ -12,6 +12,7 @@ import { AutomationsProvider } from "@/context/AutomationsProvider";
 import { SidebarItemsProvider } from "@/context/SideBarItemsProvider";
 import { Toaster } from "sonner";
 import { auth } from "@/server/auth";
+import { env } from "@/env.mjs";
 import { redirect } from "next/navigation";
 
 export default async function DashboardLayout({
@@ -36,8 +37,15 @@ export default async function DashboardLayout({
         <SidebarRail />
         <AutomationsProvider>
           <SidebarInset className="bg-neutral-200/50">
-            <div className="flex p-4 lg:pl-0 lg:pr-8">
-              <SidebarTrigger className="mx-2 mt-1" />
+            <div className="flex-1 p-4 py-0">
+              <div className="flex items-center justify-between pb-1">
+                <SidebarTrigger className="mx-2 mt-1" />
+                {env.AOC_BUILD_NO && (
+                  <div className="pr-4 text-xs font-medium">
+                    Build · {env.AOC_BUILD_NO}
+                  </div>
+                )}
+              </div>
               {children}
             </div>
           </SidebarInset>
