be7a0068eb075f31e4775647a60b8e441bbdfefa
Timothy Hobbs <timothy@hobbs.cz>
2025-07-30 14:18:55 +0000

    Expand default disk for bitswan automation servers (#86)

--- django/bitswan_backend/automations_servers/terraform/terraform.py
+++ django/bitswan_backend/automations_servers/terraform/terraform.py
@@ -65,7 +65,7 @@ resource "aws_instance" "nixos_vm" {{
   }}
 
   root_block_device {{
-    volume_size           = 20
+    volume_size           = 120
     volume_type           = "gp3"
     delete_on_termination = true
   }}
