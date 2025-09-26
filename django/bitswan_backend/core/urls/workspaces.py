1c9a9bf3ea83a56c9037b21880f1a8b754727f34
Timothy Hobbs <timothy@hobbs.cz>
2025-09-26 18:31:26 +0200

    More django refactoring

--- django/bitswan_backend/core/urls/workspaces.py
+++ django/bitswan_backend/core/urls/workspaces.py
@@ -1,12 +1,18 @@
 from django.urls import path
 
-from bitswan_backend.core.views.workspaces_merged import RegisterCLIAPIView
+from bitswan_backend.core.views.workspaces import RegisterCLIAPIView
+from bitswan_backend.core.views.workspaces import GetUserEmqxJwtsAPIView
 
 urlpatterns = [
     path(
         "cli/register/",
         RegisterCLIAPIView.as_view(),
         name="register_cli",
+    ),
+    path(
+        "user/emqx/jwts/",
+        GetUserEmqxJwtsAPIView.as_view(),
+        name="user_emqx_jwts",
     ),
 ]
 
--- django/config/api_router.py
+++ django/config/api_router.py
@@ -6,8 +6,8 @@ from bitswan_backend.core.views.groups import OrgUsersViewSet
 from bitswan_backend.core.views.groups import OrgViewSet
 from bitswan_backend.core.views.groups import UserGroupViewSet
 from bitswan_backend.users.api.views import UserViewSet
-from bitswan_backend.core.views.workspaces_merged import AutomationServerViewSet
-from bitswan_backend.core.views.workspaces_merged import WorkspaceViewSet
+from bitswan_backend.core.views.workspaces import AutomationServerViewSet
+from bitswan_backend.core.views.workspaces import WorkspaceViewSet
 from bitswan_backend.core.urls.workspaces import urlpatterns as workspaces_urlpatterns
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
