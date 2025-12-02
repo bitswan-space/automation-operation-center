e565808adca6c3f7fe93a4abeb74c36574a684e3
Tomáš Peroutka <67205912+Pero-coder@users.noreply.github.com>
2025-12-02 19:38:16 +0100

    refactor: remove unused actions

--- aoc-frontend/src/components/groups/action.ts
+++ aoc-frontend/src/components/groups/action.ts
@@ -2,29 +2,12 @@
 // All actions are commented out and replaced with simple exports
 
 import { 
-  deleteOrgGroup, 
-  addUserToGroup, 
-  removeUserFromGroup,
   addWorkspaceToGroup,
   removeWorkspaceFromGroup,
-  addAutomationServerToGroup,
-  removeAutomationServerFromGroup,
-  createOrgGroup,
-  updateOrgGroup
 } from "@/data/groups";
 
-export const deleteOrgGroupAction = async (data: { id: string }) => {
-  return await deleteOrgGroup(data.id);
-};
-
-export const addUserToGroupAction = async (data: { id: string; groupId: string }) => {
-  return await addUserToGroup(data.id, data.groupId);
-};
-
-export const removeUserFromGroupAction = async (data: { id: string; groupId: string }) => {
-  return await removeUserFromGroup(data.id, data.groupId);
-};
-
+// These actions are kept for potential future use in WorkspaceDetailSection
+// Currently commented out but may be needed when workspace groups are implemented
 export const addWorkspaceToGroupAction = async (data: { id: string; groupId: string }) => {
   return await addWorkspaceToGroup(data.id, data.groupId);
 };
@@ -32,26 +15,3 @@ export const addWorkspaceToGroupAction = async (data: { id: string; groupId: str
 export const removeWorkspaceFromGroupAction = async (data: { id: string; groupId: string }) => {
   return await removeWorkspaceFromGroup(data.id, data.groupId);
 };
-
-export const addAutomationServerToGroupAction = async (data: { id: string; groupId: string }) => {
-  return await addAutomationServerToGroup(data.id, data.groupId);
-};
-
-export const removeAutomationServerFromGroupAction = async (data: { id: string; groupId: string }) => {
-  return await removeAutomationServerFromGroup(data.id, data.groupId);
-};
-
-export const createOrUpdateOrgGroupAction = async (data: any) => {
-  if (data.id) {
-    return await updateOrgGroup(data);
-  } else {
-    return await createOrgGroup(data);
-  }
-};
-
-export type AddUserToGroupActionType = any;
-export type RemoveUserFromGroupActionType = any;
-export type AddWorkspaceToGroupActionType = any;
-export type RemoveWorkspaceFromGroupActionType = any;
-export type AddAutomationServerToGroupActionType = any;
-export type RemoveAutomationServerFromGroupActionType = any;
