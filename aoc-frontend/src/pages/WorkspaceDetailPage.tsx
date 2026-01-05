1af7f6980208dc5f09d5673d5589c695bc47b533
Tomáš Peroutka <67205912+Pero-coder@users.noreply.github.com>
2026-01-05 11:03:41 +0100

    feat: enhance ProcessesTable and WorkspaceDetailPage with improved hover effects, memoization for performance, and conditional rendering for automation buttons

--- aoc-frontend/src/pages/WorkspaceDetailPage.tsx
+++ aoc-frontend/src/pages/WorkspaceDetailPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useTitleBar } from "@/context/TitleBarProvider";
 import { useParams } from "react-router-dom";
 import { Table, Lock } from "lucide-react";
@@ -12,10 +12,21 @@ import AutomateProcessButton from "@/components/processes/AutomateProcessButton"
 const WorkspaceDetailPage = () => {
   const { id } = useParams<{ id: string }>();
   const { data: workspaceData, isLoading } = useWorkspaceByIdQuery(id);
-  const { processes, automationServers, isLoading: automationsLoading } = useAutomations();
-  const automationServer = automationServers[workspaceData?.automation_server ?? ""];
-  const workspacePipelines = automationServer?.workspaces[workspaceData?.id ?? ""]?.pipelines;
-  const workspaceProcesses = Object.values(processes).filter((process) => process.workspace_id === workspaceData?.id);
+  const { automationServers, isLoading: automationsLoading } = useAutomations();
+  const automationServer =
+    automationServers[workspaceData?.automation_server ?? ""];
+  const workspacePipelines = useMemo(
+    () =>
+      automationServer?.workspaces[workspaceData?.id ?? ""]?.pipelines ?? [],
+    [automationServer, workspaceData]
+  );
+  const workspaceProcesses = useMemo(
+    () =>
+      Object.values(
+        automationServer?.workspaces[workspaceData?.id ?? ""]?.processes ?? {}
+      ),
+    [automationServer, workspaceData]
+  );
   const { setTitle, setIcon, setButtons } = useTitleBar();
   const [dialogOpen, setDialogOpen] = useState(false);
 
@@ -24,19 +35,28 @@ const WorkspaceDetailPage = () => {
     setIcon(<Table size={24} />);
     setButtons(
       <>
-        <Button
-          variant="outline"
-          onClick={() => setDialogOpen(true)}
-        >
+        <Button variant="outline" onClick={() => setDialogOpen(true)}>
           <Lock size={16} />
           User access
         </Button>
-        <AutomateProcessButton workspaceId={workspaceData?.id} automationServerId={workspaceData?.automation_server} />
+        {workspaceProcesses.length > 0 || workspacePipelines.length > 0 ? (
+          <AutomateProcessButton
+            workspaceId={workspaceData?.id}
+            automationServerId={workspaceData?.automation_server}
+          />
+        ) : null}
       </>
-    )
-  }, [setTitle, setIcon, setButtons, workspaceData]);
+    );
+  }, [
+    setTitle,
+    setIcon,
+    setButtons,
+    workspaceData,
+    workspaceProcesses,
+    workspacePipelines,
+  ]);
 
-  if (isLoading) {
+  if (isLoading || automationsLoading) {
     return <div>Loading...</div>;
   }
 
@@ -46,15 +66,24 @@ const WorkspaceDetailPage = () => {
 
   return (
     <>
-      <ProcessListSection
-        automations={workspacePipelines}
-        processes={workspaceProcesses}
-        isLoading={automationsLoading}
-        hideWorkspaceColumn={true}
-        lastUpdated={workspaceData.updated_at}
-      />
-      <WorkspaceAccessDialog 
-        open={dialogOpen} 
+      {workspaceProcesses.length > 0 || workspacePipelines.length > 0 ? (
+        <ProcessListSection
+          automations={workspacePipelines}
+          processes={workspaceProcesses}
+          isLoading={automationsLoading}
+          hideWorkspaceColumn={true}
+          lastUpdated={workspaceData.updated_at}
+        />
+      ) : (
+        <div className="flex justify-center items-center h-40 border border-dashed m-4">
+          <AutomateProcessButton
+            workspaceId={workspaceData?.id}
+            automationServerId={workspaceData?.automation_server}
+          />
+        </div>
+      )}
+      <WorkspaceAccessDialog
+        open={dialogOpen}
         onOpenChange={setDialogOpen}
         workspaceId={workspaceData.id}
       />
