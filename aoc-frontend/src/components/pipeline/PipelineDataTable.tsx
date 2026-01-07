6bdd23d97a754cc69480d00996f718ded48da55d
monnikareh <monikarehakova333@gmail.com>
2026-01-07 16:35:34 +0100

    feat: delete the rows selected information since it was always 0 and we cant select rows

--- aoc-frontend/src/components/pipeline/PipelineDataTable.tsx
+++ aoc-frontend/src/components/pipeline/PipelineDataTable.tsx
@@ -439,10 +439,6 @@ export function PipelineDataTable(props: PipelineDataTableProps) {
         </Table>
       </div>
       <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="text-muted-foreground flex-1 text-sm">
-          {table.getFilteredSelectedRowModel().rows.length} of{" "}
-          {table.getFilteredRowModel().rows.length} row(s) selected.
-        </div>
         <div className="space-x-2">
           <Button
             variant="outline"
