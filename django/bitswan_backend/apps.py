327d7fd71e0acd46423e4d9e53ebb9234dbdc247
Tomáš Peroutka <67205912+Pero-coder@users.noreply.github.com>
2026-01-07 11:12:17 +0100

    feat: initialize MQTTService in BitswanBackendConfig to publish all groups on app readiness

--- django/bitswan_backend/apps.py
+++ django/bitswan_backend/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class BitswanBackendConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bitswan_backend'
+    
+    def ready(self):
+        from bitswan_backend.core.mqtt import MQTTService
+        mqtt_service = MQTTService()
+        mqtt_service.publish_all_groups()
