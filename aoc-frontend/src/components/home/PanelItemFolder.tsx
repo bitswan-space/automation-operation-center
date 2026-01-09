692ffb8c971a6ae03682ed69ffca4b69bec281d9
Tomáš Peroutka <67205912+Pero-coder@users.noreply.github.com>
2026-01-09 19:41:02 +0100

    refactor: simplify PanelItemFolder and enhance navigation in PanelItemsSection

--- aoc-frontend/src/components/home/PanelItemFolder.tsx
+++ aoc-frontend/src/components/home/PanelItemFolder.tsx
@@ -1,19 +1,17 @@
 import { Card, CardContent } from "../ui/card";
 import React from "react";
-import { FolderClosed, FolderOpen } from "lucide-react";
+import { FolderClosed } from "lucide-react";
 import { type RawNavItem } from "../layout/Sidebar/utils/NavItems";
-import PanelItemCard from "./PanelItemCard";
-import { DynamicIcon } from "../layout/Sidebar/DynamicIcon";
 
 type PanelItemFolderProps = {
   title: string;
   items: RawNavItem[];
+  onClick: () => void;
 };
 
 export default function PanelItemFolder(props: PanelItemFolderProps) {
-  const { title, items } = props;
+  const { title, onClick } = props;
   const [isMounted, setIsMounted] = React.useState(false);
-  const [isOpen, setIsOpen] = React.useState(false);
 
   React.useEffect(() => {
     setIsMounted(true);
@@ -24,57 +22,24 @@ export default function PanelItemFolder(props: PanelItemFolderProps) {
   }
 
   return (
-    <>
-      <Card className="h-40 rounded-xl border-border shadow-sm">
-        <CardContent className="h-full pt-4">
-          <button
-            onClick={() => setIsOpen(!isOpen)}
-            className="w-full h-full"
-          >
-            <div className="my-auto flex h-full flex-col justify-center gap-3 pt-4 text-center">
-              <div className=" mx-auto">
-                {isOpen ? (
-                  <FolderOpen size={24} className="text-neutral-900" />
-                ) : (
-                  <FolderClosed size={24} className="text-neutral-900" />
-                )}
-              </div>
-              <div className="mx-auto flex  gap-2 align-bottom">
-                <div className="mt-auto text-base font-semibold text-card-foreground">
-                  {title}
-                </div>
+    <Card className="h-40 rounded-xl border-border shadow-sm hover:cursor-pointer">
+      <CardContent className="h-full pt-4 p-1">
+        <button onClick={onClick} className="w-full h-full">
+          <div className="my-auto flex h-full flex-col justify-center gap-3 pt-4 text-center">
+            <div className="mx-auto">
+              <FolderClosed size={24} className="text-neutral-900" />
+            </div>
+            <div className="mx-auto flex w-full gap-2 align-bottom px-2">
+              <div
+                className="mt-auto text-base font-semibold text-card-foreground text-wrap break-words w-full max-h-[72px] overflow-y-hidden"
+                style={{ overflowWrap: "anywhere" }}
+              >
+                {title}
               </div>
             </div>
-          </button>
-        </CardContent>
-      </Card>
-      {isOpen && (
-        <>
-          {items?.map((item) =>
-            item.type === "folder" ? (
-              <PanelItemFolder
-                key={item.id + "folder"}
-                title={item.name}
-                items={item.children ?? []}
-              />
-            ) : (
-              <PanelItemCard
-                key={item.id + "card"}
-                icon={
-                  <DynamicIcon
-                    name={item.icon}
-                    size={24}
-                    className="text-neutral-500"
-                    title={item.icon}
-                  />
-                }
-                title={item.name}
-                url={item.link}
-              />
-            )
-          )}
-        </>
-      )}
-    </>
+          </div>
+        </button>
+      </CardContent>
+    </Card>
   );
 }
